0780d6776fc90b19633b8023bcb23505db60e1ed
helen <hhma@alumni.cpp.edu>
2020-10-04 12:13:59 -0400

    remove unnecessary import statement.

--- src/main/java/com/codespacelab/order/service/MenuService.java
+++ src/main/java/com/codespacelab/order/service/MenuService.java
@@ -7,7 +7,6 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
