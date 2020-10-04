84d530538fc6e576df5040ab2c80ec06c80e2545
helen <hhma@alumni.cpp.edu>
2020-10-04 12:41:16 -0400

    remove unnecessary import statement.

--- src/test/java/com/codespacelab/order/OrderApplicationTests.java
+++ src/test/java/com/codespacelab/order/OrderApplicationTests.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import java.util.Arrays;
