e175d37f733c91703bd3a823f557e75ea8908456
helen <hhma@alumni.cpp.edu>
2020-10-06 21:00:23 -0400

    add Zuul gateway as Feign client

--- src/main/java/com/codespacelab/order/service/UserClient.java
+++ src/main/java/com/codespacelab/order/service/UserClient.java
@@ -1,13 +1,16 @@
 package com.codespacelab.order.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("user")
+//@FeignClient("user")
+@FeignClient("zuul")
+@RibbonClient("user")
 public interface UserClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/validate")
+    @RequestMapping(method = RequestMethod.GET, value = "/user/user/validate")
     boolean isUserValid(@RequestParam(name = "id") Long id);
 }
