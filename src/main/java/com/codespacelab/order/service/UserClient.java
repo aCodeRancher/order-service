ec069076ef213862049120eec752506bd719f3b7
helen <hhma@alumni.cpp.edu>
2020-10-10 17:09:27 -0400

    add zipkin dependency

--- src/main/java/com/codespacelab/order/service/UserClient.java
+++ src/main/java/com/codespacelab/order/service/UserClient.java
@@ -1,17 +1,13 @@
 package com.codespacelab.order.service;
 
-import com.codespacelab.order.config.OrderWebSecurityConfig;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient("user")
-@FeignClient("zuul")
-@RibbonClient(name = "user")
+@FeignClient("user")
 public interface UserClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/user/validate")
+    @RequestMapping(method = RequestMethod.GET, value = "/user/validate")
     boolean isUserValid(@RequestParam(name = "id") Long id);
 }
